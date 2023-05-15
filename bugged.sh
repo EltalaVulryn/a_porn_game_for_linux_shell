500dd290d08e2a2b22e3a2ae51575f5cb475b3f1
Jonathan (Eltala) Moore <93369519+EltalaVulryn@users.noreply.github.com>
2023-05-15 23:34:16 +0000

    *no sneak peeks for right of now~*

---
+++ bugged.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "**debugger**"
+
+echo "**debugger**)"
+
+read zen
+
+if [[ $zen == "y" ]]; then
+	echo "*debugger*"
+else
+	echo  "** debugger ** "
+read Gano
+
+	exit 1
+
+if [[ $zen == "y" ]]; then
+	echo " ** debugger ** "
+else
+	echo " **debugger** "
+
+fi
+
+
+
