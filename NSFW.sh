774fbb7a6bb0412ce6c2ca668c3fc00100ba2133
Jonathan (Eltala) Moore <93369519+EltalaVulryn@users.noreply.github.com>
2023-05-16 01:28:30 +0000

    alpha, you can only do 2 things of right now

---
+++ NSFW.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+echo "hello there handsom~"
+
+echo "would you like to have some fun with me~? (Y/n)"
+
+read zen
+
+if [[ $zen == "y" ]]; then
+	echo "*giggles seductively*"
+else
+	echo "aww.... have a good day though... *souds dissapotiented*"
+
+fi
+
+sleep 2
+
+echo "*she would lead you to a room, and once you walk in, she closes the door,* would you like to help me remove my clothes~? (Y/n)"
+
+sleep 2
+
+read gano
+
+if [[ $gano == $zen ]]; then
+	echo "*she starts to remove her panties as you got to take off her brah, kissing her*"
+else
+	echo "oh... so your one that will make us do all the work aren't you~?"
+fi
+
+
+
