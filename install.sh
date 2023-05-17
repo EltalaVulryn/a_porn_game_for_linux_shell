9ac64122d769b9c4874ff54c854d2c81756a2c7f
Jonathan (Eltala) Moore <93369519+EltalaVulryn@users.noreply.github.com>
2023-05-17 04:09:34 +0000

    install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ mkdir /Games/eroticnight~
 cp ./NSFW.py /Games/eroticnight~/NSFW.py
 cp ./pussy /Games/eroticnight~/pussy
 cp ./README.md /Games/eroticnight~/README.md
-cp ./uninstall /Games/eroticnight~/uninstall
+cp ./uninstall.sh /Games/eroticnight~/uninstall.sh
