a6b6f9b70459896afb1ed8b72f4d8402fe9b9c73
Jonathan (Eltala) Moore <93369519+EltalaVulryn@users.noreply.github.com>
2023-05-19 00:52:43 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,16 +9,17 @@ if [[ $install == 'y' ]]; then
 	mkdir /Games
 	mkdir /Games/eroticnight~
 	mkdir /Games/eroticnight~/animations
+	mkdir /Games/eroticnight~/assets
 	cp ./NSFW.py /Games/eroticnight~/NSFW.py
 	cp ./pussy /Games/eroticnight~/pussy
 	cp ./README.md /Games/eroticnight~/README.md
 	cp ./uninstall.sh /Games/eroticnight~/uninstall.sh
-	cp -r ./animations /Games/eroticnight~/animations
+	cp -r ./animations /Games/eroticnight~
         cp ./animations/cum.sh /Games/eroticnight~/animations/cum.sh
 	cp ./animations/cump.sh /Games/eroticnight~/animations/cump.sh
-	cp ./animations/rbutter.sh /Games/eroticnight~/animations/r
-	butter.sh
-	cp ./animations/rbutterp.sh /Games/eroticnight~/animations/rbutterp.sh 
+	cp ./animations/rbutter.sh /Games/eroticnight~/animations/rbutter.sh
+	cp ./animations/rbutterp.sh /Games/eroticnight~/animations/rbutterp.sh
+	cp ./assets/repeatrbut.sh /Games/eroticnight~/assets/repeatrbut.sh  
 	echo "completed"
 else
 	echo "cancelled"
