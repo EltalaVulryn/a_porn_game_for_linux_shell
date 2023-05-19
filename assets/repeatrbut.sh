d69d794b83ba99afd41f43a02e1cd83049694aa4
Jonathan (Eltala) Moore <93369519+EltalaVulryn@users.noreply.github.com>
2023-05-19 00:56:35 +0000

    something to cut down on the code in python

---
+++ assets/repeatrbut.sh
@@ -0,0 +1,8 @@
+bash /Games/eroticnight~/animations/rbutter.sh
+sleep 1
+bash /Games/eroticnight~/animations/rbutter.sh
+sleep 1
+bash /Games/eroticnight~/animations/rbutter.sh
+sleep 1
+bash /Games/eroticnight~/animations/rbutter.sh
+sleep 1
